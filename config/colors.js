9fef3d0d96b35c82fb8efdf45cd3c4d5c0c8000d
punisher435 <kotharichirag1989@gmail.com>
2022-04-16 17:28:09 +0530

    f

---
+++ config/colors.js
@@ -0,0 +1,11 @@
+export default {
+    theme:'#185ADB',
+    white:'#ffffff',
+    dark:'#000000',
+    error:'#B00020',
+    grey:'grey',
+    sendermessages:'#A2DBFA',
+    receivermessages:'#F4F9F9',
+}
+
+/* #178AE5   #185ADB*/
---
+++ screens/Intro.js
@@ -0,0 +1,17 @@
+import { View, StyleSheet  } from 'react-native'
+import React from 'react'
+
+import AppText from '../components/Text';
+
+export default function Intro() {
+  return (
+    <View>
+      <AppText>Intro</AppText>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+   
+  });
+  
